cc1a9b4f7a4b0605ef9a118a7596c3e580133ce8
pr-a-nav <pranavmishra680@gmail.com>
2024-09-05 17:19:59 +0530

    execute function testing

--- agents/execute.py
+++ agents/execute.py
@@ -1,5 +1,24 @@
 import typing
 
 
-def execute( fn : typing.Callable, args : str):
-    pass
+# def execute( fn : typing.Callable, args : str):
+#     pass
+
+
+def hello():
+     return "hello"
+
+def done():
+     print("done")
+     return "done"
+
+
+def execute( list : typing.List[typing.Callable]):
+      for fn in list:
+          result = fn()
+          print(result)
+
+fn_list: typing.List[typing.Callable] =[hello,done]
+
+
+execute(fn_list)
